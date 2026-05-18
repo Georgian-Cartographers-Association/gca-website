f9bcfe2e5911dbd7ae53023686344b259c09a54e
Giorgi Kapanadze <kapan.gio777@gmail.com>
2026-05-18 10:23:58 +0400

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent.ftbs5-3.fna.fbcdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
